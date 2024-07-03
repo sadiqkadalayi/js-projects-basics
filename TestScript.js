02e25a4e786f588ed95d6ebf708a91e626f869eb
sadiq <sadiq.creatik@gmail.com>
2024-07-03 17:19:32 +0300

    2nd version

--- TestScript.js
+++ TestScript.js
@@ -2,38 +2,59 @@
 // <p id="quoteText"></p> */
 
 const quoteButton = document.getElementById('quoteButton');
-const quoteReplacePTag = document.getElementById('quoteText');
+const quoteText = document.getElementById('quoteText');
+const quoteList = document.getElementById('quoteList');
+const DigitButton = document.getElementById('DigitButton');
+const DigitText = document.getElementById('DigitText');
 
-const quotesNames = ['Mohammed sadiqali', 'Mohammed Junaid', 'Mohammed Anas', 'Mohammed K'];
-
-const QuoteNamesDis = () => {
-    const RandomNames = Math.floor(Math.random() * quotesNames.length);
-    quoteReplacePTag.innerHTML = Math.floor(Math.random()*10000);
+const DigitButtonDisply = () => {
+    const DigitButtonRandom = Math.floor(Math.random() * 10000000);
+    
+    DigitText.innerHTML = DigitButtonRandom;
 }
+DigitButton.addEventListener('click',DigitButtonDisply);
 
-const display = quoteButton.addEventListener('click', QuoteNamesDis);
+const quotes = ['Pooda moonae dineasha','kalikanda nee..','paryaipikallae ne'];
 
+const displQt = () => {
+    const randomIndexQuotes = Math.floor(Math.random() * quotes.length);
+    quoteText.innerHTML = quotes[randomIndexQuotes];
+    for (let Arr of quotes){
+        const element = document.createElement('li');
+        element.innerHTML=Arr;
+        quoteList.appendChild(element);
+        quoteButton.disabled=true;
+    }
 
+}
+const quoteDisplay = quoteButton.addEventListener('click', displQt);
 
-/* <input type="text" id="taskInput" placeholder="Enter a task">
-<button id="addButton">Add</button>
-<ul id="taskList"></ul> */
+//<input type="text" id="taskInput" placeholder="Enter a task">
+//<button id="addButton">Add</button> 
 
-const input = document.getElementById('taskInput');
-const btn = document.getElementById('addButton');
-const Listitem = document.getElementById('taskList');
+const taskInput = document.getElementById('taskInput');
+const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
 
-btn.addEventListener('click', TaskDisplay);
-
-const TaskDisplay = () => {
-    const task = input.value.trim();
-    if ( task !== "" ){
-        const TaskLi = document.createElement('li');
-        TaskLi.innerHTML = task;
-        Listitem.appendChild(TaskLi);
-        input.value = "";
+const Tododisplay = () => {
+    const task = taskInput.value.trim();
+    if (task !== ""){
+        const ele = document.createElement('li');
+        ele.innerHTML = task;
+        taskList.appendChild(ele);
+        taskInput.value = "";
     }
 }
+const LDispl = addButton.addEventListener('click', Tododisplay);
 
 
+const KtaskInput = document.getElementById('KtaskInput');
+const KaddButton = document.getElementById('KaddButton');
+const KtaskList = document.getElementById('KtaskList');
 
+const Kododisplay = () => {
+    const task = KtaskInput.value.trim();
+    if (task !== ""){
+        KtaskList.innerHTML = task;
+    }
+}
